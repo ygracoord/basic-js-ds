d4fbe29e53727bc86a027d543e83ab8105b6a072
ygracoord <ygra.pl@gmail.com>
2022-10-17 10:44:49 +0300

    feat: result stack task

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(stk) {
+    this.stk = [];
+  }
+  push(element) {
+    return this.stk.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stk.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stk.at(-1);
   }
 }
 
